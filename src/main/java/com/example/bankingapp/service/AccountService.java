a237ae31063a6c5de05165ebea4ce529f8235ad7
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-13 03:39:12 +0400

    dev

--- src/main/java/com/example/bankingapp/service/AccountService.java
+++ src/main/java/com/example/bankingapp/service/AccountService.java
@@ -2,6 +2,7 @@ package com.example.bankingapp.service;
 
 import com.example.bankingapp.entity.Account;
 import com.example.bankingapp.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.Optional;
 @Service
 public class AccountService {
 
+    @Autowired
     private AccountRepository accountRepository;
 
     public Account createAccount(Account account) {
