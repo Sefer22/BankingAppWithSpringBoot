7d6b9fc251321a35df779bb1dc0269c2f0568011
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-13 02:48:54 +0400

    first commit

---
+++ src/main/java/com/example/bankingapp/service/AccountService.java
@@ -0,0 +1,23 @@
+package com.example.bankingapp.service;
+
+import com.example.bankingapp.entity.Account;
+import com.example.bankingapp.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AccountService {
+
+    private AccountRepository accountRepository;
+
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Optional<Account> getAccount(Long id, double amount) {
+        return accountRepository.findById(id);
+    }
+
+    public Account deposit(Long id,)
+}
